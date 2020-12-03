138fd5cefc9d18968ef010686da496cf08e1642d
Yuhui-Wu <yuw119@pitt.edu>
2020-12-03 03:39:00 -0500

    extend interface

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,6 +1,6 @@
-import { createError } from './helpers/error';
-import { parseHeaders } from './helpers/headers';
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types';
+import { createError } from '../helpers/error';
+import { parseHeaders } from '../helpers/headers';
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types';
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -12,7 +12,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       request.responseType = responseType;
     }
 
-    request.open(method.toUpperCase(), url, true);
+    request.open(method.toUpperCase(), url!, true);
 
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== XMLHttpRequest.DONE) {
--- src/types/index.ts
+++ src/types/index.ts
@@ -15,7 +15,7 @@ export type Method =
   | 'OPTIONS';
 
 export interface AxiosRequestConfig {
-  url: string;
+  url?: string;
   method?: Method;
   data?: any;
   params?: any;
@@ -24,8 +24,8 @@ export interface AxiosRequestConfig {
   timeout?: number;
 }
 
-export interface AxiosResponse {
-  data: any;
+export interface AxiosResponse<T = any> {
+  data: T;
   status: number;
   statusText: string;
   headers: any;
@@ -41,4 +41,29 @@ export interface AxiosError extends Error {
   isAxiosError: boolean;
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
+
+export interface Axios {
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>;
+
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
+
+  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>;
+}
+
+export interface AxiosInstance extends Axios {
+  //function that set AxiosRequestConfig as parameter and return AxiosPromise
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>;
+
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>;
+}
